e7d7b0f7d034304fa236e9a7bfa94994fd3f202e
Anjali <yanjali1728@gmail.com>
2025-12-04 00:06:38 +0530

    test

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <body className="bg-slate-950 text-slate-100">
         <Main />
