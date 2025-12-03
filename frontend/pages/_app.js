72fd6cd7bed014362105d28d213b9c6a26077690
Anjali <yanjali1728@gmail.com>
2025-12-04 00:21:09 +0530

    Added tab head

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,15 @@
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Head>
+        <title>Anjali Yadav</title>   
+        <meta name="description" content="Sequence Analysis Platform" />
+      </Head>
 
+      <Component {...pageProps} />
+    </>
+  );
+}
